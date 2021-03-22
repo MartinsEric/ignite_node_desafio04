5c2bc33bd95de3dff52e0d87336c1a4848fe5089
Eric Martins <eric.martins@radixeng.com.br>
2021-03-22 16:07:30 -0300

    implementing Controllers

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,21 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.headers;
+    let id: string = null;
+
+    if (user_id instanceof Array) {
+      id = user_id[0];
+    } else {
+      id = user_id;
+    }
+
+    try {
+      const user = this.listAllUsersUseCase.execute({ user_id: id });
+      return response.json(user);
+    } catch (error) {
+      return response.status(400).json({ error: error.message });
+    }
   }
 }
 
